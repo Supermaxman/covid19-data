7f55103c2ef0f090f84f0cb167df73d5e18e207b
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-20 18:42:24 -0600

    Initial commit for model

--- experiments/covid-lies/5.sh
+++ experiments/covid-lies/5.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #bash 5-1.sh
-bash 5-2.sh
-bash 5-3.sh
-bash 5-4.sh
-bash 5-5.sh
+bash experiments/covid-lies/5-2.sh
+bash experiments/covid-lies/5-3.sh
+bash experiments/covid-lies/5-4.sh
+bash experiments/covid-lies/5-5.sh
