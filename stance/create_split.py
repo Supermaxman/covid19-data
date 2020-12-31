5245324622910e80b1cbd283641cc504370afd27
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-31 00:02:31 -0600

    More experiments

--- stance/create_split.py
+++ stance/create_split.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 	parser.add_argument('-o', '--output_path', required=True)
 	parser.add_argument('-s', '--splits', default=5, type=float)
 	parser.add_argument('-d', '--seed', default=0, type=float)
-	parser.add_argument('-t', '--split_type', default='group', type=float)
+	parser.add_argument('-t', '--split_type', default='group')
 	args = parser.parse_args()
 
 	np.random.seed(args.seed)
